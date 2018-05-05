688c1fcdbc38dafdb9e33de9b521c4fbb0641e3f
unknown <inq.williamc@gmail.com>
2018-05-05 07:17:56 -0700

    Initial commit

---
+++ app/src/main/java/com/example/rosy/trabajo_final/Board.java
@@ -0,0 +1,6 @@
+package com.example.rosy.trabajo_final;
+
+import android.app.Activity;
+
+public class Board extends Activity {
+}
---
+++ app/src/main/java/com/example/rosy/trabajo_final/SplashActivity.java
@@ -0,0 +1,29 @@
+package com.example.rosy.trabajo_final;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
+public class SplashActivity extends Activity {
+    private final int DURACION_SPLASH = 2000;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.activity_spash);
+
+        new Handler().postDelayed(new Runnable(){
+            public void run(){
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            };
+        }, DURACION_SPLASH);
+    }
+}
